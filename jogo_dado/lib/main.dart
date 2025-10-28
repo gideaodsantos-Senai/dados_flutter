f5f94d82d271316d8fcf26a3ca4b4fe650dc1635
Gideão dos Santos <gideao.d.santos@aluno.senai.br>
2025-10-28 17:09:51 -0300

    criação de lançamentos de jogos e resultado dos jogos

--- jogo_dado/lib/main.dart
+++ jogo_dado/lib/main.dart
@@ -113,7 +113,7 @@ class TelaJogodeDados extends StatefulWidget {
   //Variáveis finais que armazenam os nomes recebidos da dela anterior
   final String nomeJogador1;
   final String nomeJogador2;
-
+  //TETLAJOGODEDADOS é o corpo de um robô
   const TelaJogodeDados ({
     super.key,
     //o required garante que esses valores devem ser passados.
@@ -124,5 +124,62 @@ class TelaJogodeDados extends StatefulWidget {
     @override
     //Ei tio flutter, quando essa tela for criada, use essa classe chamada _EstadoTelaJogoDeDados
     //para guardar e controlar o estado dela
+    //ESTADOTELAJOGODEJOGADOR é o cérebro do robô que guarda o que está acontecendo.
+    //o createstate é o botão que coloca o cerébro dentro do robô
     State<TelaJogodeDados> createState() => _EstadoTelaJogoDeDados();
+}
+
+class _EstadoTelaJogoDeDados extends State<TelaJogodeDados>{
+  final Random _aleatorio = Random(); //gerador de números aleatórios
+  //Lista dos 3 valores de cada jogador.
+  List<int> _lancamentosJogador1 = [1,1,1];
+  List<int> _lancamentosJogador2 = [1,1,1];
+  String _mensagemResultado = ''; //Mensagem de resultado da rodada.
+
+  //Mapear as associações do número dado referente ao link
+  final Map<int, String> imagensDados = {
+    1: 'https://i.imgur.com/1xqPfjc.png&#39',
+    2: 'https://i.imgur.com/5ClIegB.png&#39',
+    3: 'https://i.imgur.com/hjqY13x.png&#39',
+    4: 'https://i.imgur.com/CfJnQt0.png&#39',
+    5: 'https://i.imgur.com/6oWpSbf.png&#39',
+    6: 'https://i.imgur.com/drgfo7s.png&#39',
+  };
+
+  // Lógica da pontuação: verifica combinações para aplicar os multiplicadores.
+  int _calculadorPontuacao(List<int> lancamentos){
+    //reduce percorre toda a lista somando tudo
+    final soma = lancamentos.reduce((a,b) => a + b);
+    // [4,4,1] > 4 + 4 = 8 > 8 + 1 = 9 > soma = 9
+    final valoresUnicos = lancamentos.toSet().length;
+    //toSet remove repetidos
+    if (valoresUnicos == 1){ //Ex: [5,5,5]. Três iguais = 3x a soma
+      return soma * 3;
+    } else if (valoresUnicos == 2){ //Ex: [4,4,1] dois iguais = 2x a soma
+      return soma * 2;
+    } else { // Ex: [1,3,6]. Todos diferentes = soma pura.
+      return soma;
+    }
+  }
+  //Função chamada pelo botão para lançar os dados
+  void _lancarDados(){ //eu uso o sublinhado "_" significa que ela é privada, só pode ser usada
+     //dentro dessa classe
+     //comando crucial p/ forçar atualização da tela
+    setState(() {
+      _lancamentosJogador1 = List.generate(3, (_) => _aleatorio.nextInt(6) + 1);
+      _lancamentosJogador2 = List.generate(3, (_) => _aleatorio.nextInt(6) + 1);
+
+      final pontuacao1 = _calculadorPontuacao(_lancamentosJogador1);
+      final pontuacao2 = _calculadorPontuacao(_lancamentosJogador2);
+
+      if (pontuacao1 > pontuacao2) {
+        _mensagemResultado = '${widget.nomeJogador1} Venceu! ($pontuacao1 x $pontuacao2)';
+      } else if (pontuacao2 > pontuacao1) {
+        _mensagemResultado = '${widget.nomeJogador2} Venceu! ($pontuacao2 x $pontuacao1)';
+      } else {
+        _mensagemResultado = 'Empate! Joguem novamente.';
+      }
+    });
+  }
+
 }
