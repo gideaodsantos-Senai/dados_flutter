2d929edb9b01affaefb0b317bcbc7bc4b46c4d9a
Gideão dos Santos <gideao.d.santos@aluno.senai.br>
2025-10-28 16:06:04 -0300

    Criando validação para começar o jogo

--- jogo_dado/lib/main.dart
+++ jogo_dado/lib/main.dart
@@ -40,5 +40,68 @@ class TelaConfiguracaoJogadores extends StatefulWidget {
 class _EstadoTelaConfiguracaoJogadores extends State<TelaConfiguracaoJogadores>{
   //Chave Global para identificar e validar o widget
   //final é uma palavra chave do dart para criar uma variável que só recebe valor uma vez
+  //FormSate é o esatdo interno desse formulário, é a parte que sabe o que está digitado e consegue validar os campos
   final _chaveFormulario = GlobalKey<FormState>();
+  //Controladores para pegar o texto digitado nos campos
+  final TextEditingController _controladorJogador1 = TextEditingController();
+  final TextEditingController _controladorJogador2 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Configurações dos Jogadores"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16), //Espaçamento Interno
+        child: Form(
+          key: _chaveFormulario, //Associando a chave GlobalKey ao formulário
+          child: Column(
+            children: [
+              //Campo de texto para o jogador n°1
+              TextFormField(
+                controller: _controladorJogador1, //Liga o input ao controlador
+                decoration: const InputDecoration(labelText: "Nome Jogador 1"),
+                validator: (valor) => valor!.isEmpty ? "Digite o nome" : null,
+                // condição ? valor_se_verdadeiro : valor_se_falso
+                //Se o campo estiver vazio, mostre o texto "Digite um nome".
+              ),
+              const SizedBox(height: 20),
+              //Campo de texto para o jogador n°2
+              TextFormField(
+                controller: _controladorJogador2, //Liga o input ao controlador
+                decoration: const InputDecoration(labelText: "Nome Jogador 2"),
+                validator: (valor) => valor!.isEmpty ? "Digite o nome" : null,
+                // condição ? valor_se_verdadeiro : valor_se_falso
+                //Se o campo estiver vazio, mostre o texto "Digite um nome".
+              ),
+              const Spacer(), //Ocupar o espaço vertical disponível, empurrando o botão p/ baixo
+              //Fazer um botão para iniciar o jogo
+              ElevatedButton(
+                onPressed: (){
+                  //Checar se o formulário está válido(se os campos foram preenchidos)
+                  if (_chaveFormulario.currentState!.validate()){
+                    //Navega para a próxima tela
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        //Cria a tela do jogo, PASSANDO os nomes digitador como Parâmetros.
+                        builder: (context) => TelaJogodeDados(
+                          nomeJogador1: _controladorJogador1.text,
+                          nomeJogador2: _controladorJogador2.text,
+                        )
+                      )
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(minimumSize: const Size(double.infinity, 50)),
+                //Botão de largura total.
+                child: const Text("Inicial Jogo"),
+                )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
